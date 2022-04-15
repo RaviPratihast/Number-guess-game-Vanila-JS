7ad92351c9c889da93dec8b2209d9570e3e2c4bf
RAVI SHANKAR PRATIHAST <75324945+RaviPratihast@users.noreply.github.com>
2022-04-15 11:57:49 +0530

    commit

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// checking the display
+
+// console.log(document.querySelector('.answer').textContent);
+
+document.querySelector('.answer').textContent = 'correct Answer';
+
+// testing the secret number in the console.
+console.log(document.querySelector('.secret-number').textContent);
+
+// document
+//   .querySelector('.check')
+//   .addEventListener('click', () => console.log('adad'));
+
+// function check() {
+//   console.log('clicked');
+// }
+
+// document.querySelector('.check').addEventListener('click',function(){
+//   if(!user)
+// });
