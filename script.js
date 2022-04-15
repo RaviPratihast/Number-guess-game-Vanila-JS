b6bbbf8282fd9017e9fd6c43a71978a81eea52bf
RAVI SHANKAR PRATIHAST <75324945+RaviPratihast@users.noreply.github.com>
2022-04-15 13:09:33 +0530

    commit

--- script.js
+++ script.js
@@ -4,19 +4,33 @@
 
 // console.log(document.querySelector('.answer').textContent);
 
-document.querySelector('.answer').textContent = 'correct Answer';
+// document.querySelector('.message').textContent = 'Correct Answer';
+// document.querySelector('.message').textContent = 'Wrong Input';
 
 // testing the secret number in the console.
-console.log(document.querySelector('.secret-number').textContent);
 
-// document
-//   .querySelector('.check')
-//   .addEventListener('click', () => console.log('adad'));
+// const test = (document.querySelector('.number').textContent = 12);
+// console.log(document.querySelector('.number').textContent);
+// console.log(typeof test);
 
-// function check() {
-//   console.log('clicked');
-// }
+// testing the input part and the button.
 
-// document.querySelector('.check').addEventListener('click',function(){
-//   if(!user)
-// });
+/*
+
+document
+  .querySelector('.check')
+  .addEventListener('click', () => console.log('adad'));
+
+function check() {
+  console.log('clicked');
+}
+
+document.querySelector('.check').addEventListener('click',function(){
+  if(!user)
+});
+
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  console.log('clicked!');
+});
